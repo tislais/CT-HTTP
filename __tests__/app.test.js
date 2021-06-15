e7ac32665453f7bf1c954af5dde1bc57aeda17f7
Tis Lais <tistististis@gmail.com>
2021-06-14 19:27:10 -0700

    add parseRequest function with passing tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,6 +1,27 @@
-const request = require('supertest');
-const app = require('../lib/app');
+// const request = require('supertest');
+// const app = require('../lib/app');
 
-describe('app routes', () => {
+// describe('app routes', () => {
+//   it('takes rawRequest and returns an object with body, method, and path', () => {
+    
+//   });
+// });
 
-});
+
+
+// if(path === '/') {
+//   // GET plain text "hi"
+//   console.log('plain text hi');
+// } else if(path === '/echo') {
+//   // POST status code 200 and plain text with the request body
+//   console.log('POST status code 200 and plain text with the request body');
+// } else if(path === '/red') {
+//   // GET	html with an h1 and the word red
+//   console.log('GET html with an h1 and the word red');
+// } else if(path === '/green') {
+//   // GET	html with an h1 and the word green
+//   console.log('GET html with an h1 and the word green');
+// } else if(path === '/blue') {
+//   // GET	html with an h1 and the word blue
+//   console.log('GET html with an h1 and the word blue');
+// }
