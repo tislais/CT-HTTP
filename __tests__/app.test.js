a6bf44e0514e7316e0c274f4449e32c7f3163325
Tis Lais <tistististis@gmail.com>
2021-06-15 11:26:13 -0700

    refactor /red /green /blue GET to test for content type

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -23,19 +23,23 @@ describe('app routes', () => {
   it('/red GET html with an h1 and the word red', async () => {
     const actual = await request(app).get('/red');
     const expected = '<h1>red</h1>';
+
     expect(actual.text).toEqual(expected);
+    expect(actual.type).toEqual('text/html');
   });
 
   it('/green GET html with an h1 and the word green', async () => {
     const actual = await request(app).get('/green');
     const expected = '<h1>green</h1>';
     expect(actual.text).toEqual(expected);
+    expect(actual.type).toEqual('text/html');
   });
 
   it('/blue GET html with an h1 and the word blue', async () => {
     const actual = await request(app).get('/blue');
     const expected = '<h1>blue</h1>';
     expect(actual.text).toEqual(expected);
+    expect(actual.type).toEqual('text/html');
   });
 });
 
