e2d101164bbd65701cb4e98cc063de7104448e79
Tis Lais <tistististis@gmail.com>
2021-06-15 10:43:45 -0700

    add passing /red and /green POST tests and routes

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -12,15 +12,22 @@ describe('app routes', () => {
   it('/echo	POST status code 200 and plain text with the request body', async () => {
     const actual = await request(app).post('/echo').send('test body');
     const expected = 'test body';
+    
+    expect(actual.text).toEqual(expected);
+    expect(actual.status).toEqual(200);
+  });
+
+  it('/red GET html with an h1 and the word red', async () => {
+    const actual = await request(app).get('/red');
+    const expected = '<h1>red</h1>';
     expect(actual.text).toEqual(expected);
   });
 
-  // it('/red GET html with an h1 and the word red', async () => {
-
-  // });
-
-  
-  
+  it('/green GET html with an h1 and the word green', async () => {
+    const actual = await request(app).get('/green');
+    const expected = '<h1>green</h1>';
+    expect(actual.text).toEqual(expected);
+  });
 });
 
 
