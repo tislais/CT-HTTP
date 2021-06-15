a6074a8ae8bf6fb3b0ddfee6513ea8f29587a987
Tis Lais <tistististis@gmail.com>
2021-06-15 10:15:45 -0700

    add passing /echo POST test and route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,14 +3,21 @@ const app = require('../lib/app');
 
 describe('app routes', () => {
 
-  // GET plain text "hi"
   it('/	GET	plain text "hi"', async () => {
     const actual = await request(app).get('/');
     const expected = 'hi';
     expect(actual.text).toEqual(expected);
   });
 
-  
+  it('/echo	POST status code 200 and plain text with the request body', async () => {
+    const actual = await request(app).post('/echo').send('test body');
+    const expected = 'test body';
+    expect(actual.text).toEqual(expected);
+  });
+
+  // it('/red GET html with an h1 and the word red', async () => {
+
+  // });
 
   
   
