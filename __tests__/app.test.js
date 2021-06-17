d458c0aceec2b781e65186cb489286505669a875
Tis Lais <tistististis@gmail.com>
2021-06-17 09:16:49 -0700

    add failing test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -43,6 +43,26 @@ describe('app routes', () => {
     expect(actual.text).toEqual(expected);
     expect(actual.type).toEqual('text/html');
   });
+
+  
+  it('/index.html returns file contents', async () => {
+    const actual = await request(app).get('/index.html');
+    const expected = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Index</title>
+    </head>
+    <body>
+      <h1>Index.html</h1>
+    </body>
+    </html>`;
+    
+    expect(actual).toEqual(expected);
+  });
+
 });
 
 
